556c515a7f63346f93c576ea69e6a7b441ed579d
Khac Van <52000868@student.tdtu.edu.vn.com>
2024-03-30 14:33:49 +0700

    Update first commit

---
+++ src/test/java/com/easywork/parent/EasyworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easywork.parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
